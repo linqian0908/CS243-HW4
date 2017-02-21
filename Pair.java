2314e3c277bff8ab9885650264c73e639e4b3b2a
linqian <linqian0908@gmail.com>
2017-02-20 18:04:56 -0800

    mod pair

--- Pair.java
+++ Pair.java
@@ -1,69 +1,53 @@
 package submit;
 
 import java.util.*;
-/**
- * Container to ease passing around a tuple of two objects. This object provides a sensible
- * implementation of equals(), returning true if equals() is true on each of the contained
- * objects.
- */
-public class Pair<F, S> {
-    public final F first;
-    public final S second;
 
-    /**
-     * Constructor for a Pair.
-     *
-     * @param first the first object in the Pair
-     * @param second the second object in the pair
-     */
-    public Pair(F first, S second) {
-        this.first = first;
-        this.second = second;
+public class Pair<L, R>
+{
+    public final L left;
+    public final R right;
+ 
+    public Pair(L left, R right)
+    {
+        this.left = left;
+        this.right = right;
     }
-
-    /**
-     * Checks the two objects for equality by delegating to their respective
-     * {@link Object#equals(Object)} methods.
-     *
-     * @param o the {@link Pair} to which this one is to be checked for equality
-     * @return true if the underlying objects of the Pair are both considered
-     *         equal
-     */
-    
-    public F getKey() {
-        return first;
+ 
+    public L getKey()
+    {
+        return left;
     }
-    
-    public S getValue() {
-        return second;
+ 
+    public R getValue()
+    {
+        return right;
     }
-    
+ 
     @Override
-    public boolean equals(Object o) {
-        if (!(o instanceof Pair)) {
+    public int hashCode()
+    {
+        return right.hashCode() ^ right.hashCode();
+    }
+ 
+    @Override
+    public boolean equals(Object o)
+    {
+        if (o == null)
+        {
             return false;
         }
-        Pair<?, ?> p = (Pair<?, ?>) o;
-        return Object.equals(p.first, first) && Object.equals(p.second, second);
+        if (!(o instanceof Pair))
+        {
+            return false;
+        }
+        Pair pairo = (Pair) o;
+        return this.left.equals(pairo.getKey()) &&
+                this.right.equals(pairo.getValue());
     }
-
-    /**
-     * Compute a hash code using the hash codes of the underlying objects
-     *
-     * @return a hashcode of the Pair
-     */
+ 
     @Override
-    public int hashCode() {
-        return (first == null ? 0 : first.hashCode()) ^ (second == null ? 0 : second.hashCode());
-    }
-
-    /**
-     * Convenience method for creating an appropriately typed pair.
-     * @param a the first object in the Pair
-     * @param b the second object in the pair
-     * @return a Pair that is templatized with the types of a and b
-     */
-    public static <A, B> Pair <A, B> create(A a, B b) {
-        return new Pair<A, B>(a, b);
+    public String toString()
+    {
+        return left + "," + right;
     }
 }
